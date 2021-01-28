688170222164783028e8cdadd232cabf93be9900
Jean-Damien Durand <jeandamiendurand@free.fr>
2021-01-28 13:25:27 +0100

    fastcall support

---
+++ test/bin/test.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <cmake_utils.h>
+
+int main() {
+  f("test");
+  exit(0);
+}
---
+++ test/include/cmake_utils.h
@@ -0,0 +1,15 @@
+#ifndef CMAKE_UTILS_H
+#define CMAKE_UTILS_H
+
+#include <cmake_utils/export.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  cmake_utils_EXPORT void cmake_utils_FASTCALL f(char *s);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CMAKE_UTILS_H */
+
